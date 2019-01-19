9c025e4fb5f25af836217c97058d42b4a0daca1c
chenrx <beidaojiaze@163.com>
2019-01-19 23:25:43 +0800

    auto-expanding textarea

---
+++ HTML5/Auto-Expanding textarea/index.js
@@ -0,0 +1,17 @@
+import computedVariables from 'https://unpkg.com/computed-variables/index.es.js'
+
+// Set --expand to match <textarea>'s scrollHeight
+computedVariables(
+  '--expand',
+  (value, event, tag) => {
+    tag.style.height = 'inherit'
+    const height = tag.scrollHeight
+    tag.style.height = ''
+    return height + 'px'
+  },
+  'textarea',
+  ['input', 'paste', 'blur', 'reprocess']
+)
+
+// Process variable right away
+document.querySelector('textarea').dispatchEvent(new Event('reprocess'))
